05afb74682a97f0da161fa715a3bc5088d56aa65
ken@arch <ken@arch>
2022-05-29 21:57:27 +0800

    created pytest-docker for postgresql

--- tests/test_dvc/test_core/test_database/test_postgres.py
+++ tests/test_dvc/test_core/test_database/test_postgres.py
@@ -1,33 +1,41 @@
+import docker
 import pytest
 import requests
+import psycopg2
 
 from requests.exceptions import ConnectionError
 
 
 def is_responsive(url):
     try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return True
-    except ConnectionError:
+        psycopg2.connect(url)
+    except Exception as e:
         return False
+    else:
+        return True
 
 
 @pytest.fixture(scope="session")
-def http_service(docker_ip, docker_services):
+def postgres_service(docker_ip, docker_services):
     """Ensure that HTTP service is up and responsive."""
 
     # `port_for` takes a container port and returns the corresponding host port
-    port = docker_services.port_for("httpbin", 80)
-    url = "http://{}:{}".format(docker_ip, port)
+    # docker_services.start('postgres_db')
+
+    port = docker_services.port_for("postgres_db", 5432)
+    username = 'test'
+    password = 'test'
+    db = 'test'
+    url = f"postgresql://{username}:{password}@{docker_ip}:{port}/{db}"
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
     )
     return url
 
 
-def test_status_code(http_service):
-    status = 418
-    response = requests.get(http_service + "/status/{}".format(status))
-
-    assert response.status_code == status
+@pytest.mark.usefixtures('postgres_service')
+class TestSQLFileExecutor:
+    def test__connection(self,
+                         postgres_service
+                         ):
+        assert False, f"URL is {postgres_service}"
