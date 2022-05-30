8b6c41bee3fa850b22269c0898c43aa26e1a7607
ken@arch <ken@arch>
2022-05-30 15:16:47 +0800

    moved all arrange steps to fixture

--- tests/fixtures/config_service.py
+++ tests/fixtures/config_service.py
@@ -7,10 +7,11 @@ from unittest import mock
 
 # Import dvc
 from dvc.core.config import ConfigFileReader
+from dvc.core.database import SupportedDatabaseFlavour
 
 
 @pytest.fixture()
-def dummy_user_configuration_postgres_dict() -> Dict:
+def dummy_user_configuration_with_supported_db_flavour() -> Dict:
     """
     Fixture of User Configruration
     """
@@ -22,36 +23,77 @@ def dummy_user_configuration_postgres_dict() -> Dict:
             "host": "localhost",
             "port": 5432,
             "dbname": "superman_db",
-            "dbflavour": "postgres"
+            "dbflavour": SupportedDatabaseFlavour.Postgres.value
+        }
+    }
+    return DUMMY_USER_CONFIG_FILE
+
+
+@pytest.fixture()
+def dummy_user_configuration_with_unsupported_db_flavour() -> Dict:
+    """
+    Fixture of User Configruration
+    """
+    DUMMY_USER_CONFIG_FILE: Dict = {
+        "database_revision_sql_files_folder": "sample_revision_sql_files",
+        "credentials": {
+            "user": "peter_parker",
+            "password": "1234",
+            "host": "localhost",
+            "port": 5432,
+            "dbname": "superman_db",
+            "dbflavour": "UNSUPPORTED DATABASE FLAVOUR!!!!!"
         }
     }
     return DUMMY_USER_CONFIG_FILE
 
 
 @pytest.fixture
-def dummy_config_file_path(
+def dummy_existing_config_file_path(
         tmp_path,
-        dummy_user_configuration_postgres_dict):
+        dummy_user_configuration_with_supported_db_flavour):
     """
     Set up the dummy config file
     Yield the dummy config file path
     Delete the dummy config file afterwards
     """
-    dummy_config_file_path = tmp_path.joinpath('dummy_config_file_path.yaml')
+    dummy_existing_config_file_path = tmp_path.joinpath('dummy_existing_config_file_path.yaml')
     # Set up
-    with open(dummy_config_file_path, 'w') as dummy_config_file:
-        logging.info(f"creating file {dummy_config_file_path}")
-        yaml.dump(dummy_user_configuration_postgres_dict, dummy_config_file, default_flow_style=False)
-        yield dummy_config_file_path
+    with open(dummy_existing_config_file_path, 'w') as dummy_config_file:
+        logging.info(f"creating file {dummy_existing_config_file_path}")
+        yaml.dump(dummy_user_configuration_with_supported_db_flavour, dummy_config_file, default_flow_style=False)
+        yield dummy_existing_config_file_path
 
     # Tear down
-    logging.info(f"deleting file {dummy_config_file_path}")
-    dummy_config_file_path.unlink()
+    logging.info(f"deleting file {dummy_existing_config_file_path}")
+    dummy_existing_config_file_path.unlink()
+
+
+@pytest.fixture
+def dummy_absent_config_file_path(
+        tmp_path,
+        dummy_user_configuration_with_supported_db_flavour):
+    """
+    Return path to a non-existing config file
+    """
+    dummy_absent_config_file_path = tmp_path.joinpath('dummy_absent_config_file_path.yaml')
+
+    # Set up: Remove the file if it exists
+    if dummy_absent_config_file_path.is_file():
+        logging.info(f"File {dummy_absent_config_file_path} is found. Deleting...")
+        dummy_absent_config_file_path.unlink()
+
+    yield dummy_absent_config_file_path
+
+    # Tear down: Remove the file if it exists
+    if dummy_absent_config_file_path.is_file():
+        logging.info(f"File {dummy_absent_config_file_path} is found. Deleting...")
+        dummy_absent_config_file_path.unlink()
 
 
 @pytest.fixture()
-def dummy_config_file_reader(
-        dummy_user_configuration_postgres_dict
+def dummy_config_file_reader_with_supported_db_flavour(
+        dummy_user_configuration_with_supported_db_flavour
 ):
     """
     Yield dummy config file reader with user_config property patched
@@ -59,5 +101,19 @@ def dummy_config_file_reader(
 
     with mock.patch('dvc.core.config.ConfigFileReader.user_config',
                     new_callable=mock.PropertyMock,
-                    return_value=dummy_user_configuration_postgres_dict) as mock_user_config:
+                    return_value=dummy_user_configuration_with_supported_db_flavour) as mock_user_config:
+        yield ConfigFileReader()
+
+
+@pytest.fixture()
+def dummy_config_file_reader_with_unsupported_db_flavour(
+        dummy_user_configuration_with_unsupported_db_flavour
+):
+    """
+    Yield dummy config file reader with user_config property patched
+    """
+
+    with mock.patch('dvc.core.config.ConfigFileReader.user_config',
+                    new_callable=mock.PropertyMock,
+                    return_value=dummy_user_configuration_with_unsupported_db_flavour) as mock_user_config:
         yield ConfigFileReader()
--- tests/test_dvc/test_core/test_config.py
+++ tests/test_dvc/test_core/test_config.py
@@ -13,57 +13,47 @@ from dvc.core.exception import RequestedDatabaseFlavourNotSupportedException
 
 
 class TestConfigFileWriter:
+
     def test__write_dummy_user_configuration(
             self,
-            dummy_user_configuration_postgres_dict,
-            tmp_path,
+            dummy_user_configuration_with_supported_db_flavour,
+            dummy_absent_config_file_path,
     ):
         """
-        GIVEN a dummy config file with dummy user configuration,
+        GIVEN a non-existing config file path
         WHEN ConfigFileReader.user_config is called
         THEN check dummy user configuration is returned
         """
-        # Arrange
-        dummy_config_file_name: str = "another_dummy_config_file.yaml"
-        dummy_config_file_path: Path = tmp_path.joinpath(dummy_config_file_name)
-
         # Action
-        assert not dummy_config_file_path.is_file()
-
-        # Action
-        ConfigFileWriter(dummy_config_file_path).write_to_yaml()
+        ConfigFileWriter(dummy_absent_config_file_path).write_to_yaml()
 
         # Assert
-        assert dummy_config_file_path.is_file()
-
-        # TearDown
-        dummy_config_file_path.unlink()
+        assert dummy_absent_config_file_path.is_file()
 
 
 class TestConfigFileReader:
     def test__return_expected_user_config(
             self,
-            dummy_user_configuration_postgres_dict,
-            dummy_config_file_path: Path,
+            dummy_user_configuration_with_supported_db_flavour,
+            dummy_existing_config_file_path,
     ):
         """
         GIVEN a dummy config file with dummy user configuration,
         WHEN ConfigFileReader.user_config is called
         THEN check dummy user configuration is returned
         """
-
         # Action
-        user_config = ConfigFileReader(dummy_config_file_path).user_config
+        user_config = ConfigFileReader(dummy_existing_config_file_path).user_config
 
         # Assert
-        assert user_config == dummy_user_configuration_postgres_dict
+        assert user_config == dummy_user_configuration_with_supported_db_flavour
 
 
 class TestDatabaseRevisionFilesManager:
 
     def test__validate_database_revision_sql_files__raise_InvalidDatabaseRevisionFilesException_with_status_101(
             self,
-            dummy_config_file_reader
+            dummy_config_file_reader_with_supported_db_flavour
     ):
         pass
         # assert False, dummy_config_file_reader.user_config
@@ -73,26 +63,22 @@ class TestDatabaseConnectionFactory:
 
     def test__raise_requested_database_not_supported_exception(
             self,
-            dummy_user_configuration_postgres_dict,
+            dummy_config_file_reader_with_unsupported_db_flavour,
     ):
         """
         GIVEN a fake database flavour
         WHEN DatabaseConnectionFactory.validate_requested_database_flavour is called
         THEN assert RequestedDatabaseFlavourNotSupportedException is raised
         """
-        # Arrange
-        requested_database_flavour = 'fake_database_flavour'
-
-        with mock.patch('dvc.core.config.ConfigFileReader.requested_db_flavour',
-                        new_callable=PropertyMock) as mock_requested_db_flavour:
-            config_file_reader = ConfigFileReader()
-            mock_requested_db_flavour.return_value = requested_database_flavour
-            with pytest.raises(RequestedDatabaseFlavourNotSupportedException) as exc_info:
-                DatabaseConnectionFactory(config_file_reader).validate_requested_database_flavour()
+        # Assert
+        with pytest.raises(RequestedDatabaseFlavourNotSupportedException) as exc_info:
+            # Act
+            DatabaseConnectionFactory(
+                dummy_config_file_reader_with_unsupported_db_flavour).validate_requested_database_flavour()
 
     def test__pass_user_credentials_to_connect_as_kwargs(
             self,
-            dummy_user_configuration_postgres_dict,
+            dummy_config_file_reader_with_supported_db_flavour,
     ):
         """
         GIVEN patched psycopg2.connect
@@ -101,23 +87,18 @@ class TestDatabaseConnectionFactory:
         """
         # Arrange
         with mock.patch('psycopg2.connect') as mock_connect:
-            with mock.patch('dvc.core.config.ConfigFileReader.user_config',
-                            new_callable=PropertyMock) as mock_user_config:
-                # Arrange
-                config_file_reader = ConfigFileReader()
-                mock_user_config.return_value = dummy_user_configuration_postgres_dict
-                # Act
-                conn = DatabaseConnectionFactory(config_file_reader=config_file_reader).conn
+            # Act
+            conn = DatabaseConnectionFactory(config_file_reader=dummy_config_file_reader_with_supported_db_flavour).conn
 
-                # Assert
-                expects_args = {
-                    "dbname": 'superman_db',
-                    "user": 'peter_parker',
-                    "password": '1234',
-                    "port": 5432,
-                    "host": 'localhost',
-                }
-                mock_connect.assert_called_once()
-                mock_connect.assert_called_with(
-                    **expects_args
-                )
+            # Assert
+            expects_args = {
+                "dbname": 'superman_db',
+                "user": 'peter_parker',
+                "password": '1234',
+                "port": 5432,
+                "host": 'localhost',
+            }
+            mock_connect.assert_called_once()
+            mock_connect.assert_called_with(
+                **expects_args
+            )
