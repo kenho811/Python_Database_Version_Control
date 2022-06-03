387e6433853df76ffedf030a0328769de13a4eb7
ken@arch <ken@arch>
2022-06-04 02:06:44 +0800

    updated demo_script.sh with retry

--- demo_assets/demo_script.sh
+++ demo_assets/demo_script.sh
@@ -1,12 +1,29 @@
 #!sh
-#################
-echo "Waiting for 10 seconds until db backend is ready"
-sleep 10
+
+# Retries a command on failure.
+# $1 - the max number of attempts
+# $2... - the command to run
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -i attempt_num=1
+    until "$@"
+    do
+        if ((attempt_num==max_attempts))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+            sleep $((attempt_num++))
+        fi
+    done
+}
 
 #################
 echo "Reading Config from environment variables"
 echo "Now initialising dvc configurations"
-dvc db init
+# Retry several times for first command
+retry 10 dvc db init
 echo "Now upgrading db"
 dvc db upgrade
 echo "Now upgrading db"
